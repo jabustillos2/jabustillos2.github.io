0d03dc5f4b72adda5d636f41b7385ae35fcc88b2
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-11-16 13:39:19 -0700

    n

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -8,7 +8,7 @@ console.log('My javascript is being read.');
 /* calculate windchill*/
 
 function buildWC(speed, temp) {
-    const dailyTemp = document.getElementById('feelTemp');
+    const dailyTemp = document.getElementById('dailyTemp');
 
     /*compute Windchill*/
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
