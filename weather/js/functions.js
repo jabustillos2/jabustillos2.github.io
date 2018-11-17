45834dc11c60695845d701a6122f1ee86f2439f7
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-11-17 14:12:27 -0700

    m

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -6,6 +6,10 @@
 const temp = 31;
 const speed = 5;
 buildWC(speed,temp);
+
+const direction = "N"; //Set your own value
+windDial(direction);
+
 /* calculate windchill*/
 
 function buildWC(speed, temp) {
@@ -27,3 +31,104 @@ console.log(wc);
 /* wc='feels like ' + wc+'F';*/
 feelTemp.innerHTML = wc;
 }
+
+// Wind Dial Function
+function windDial(direction) {
+    const dial = document.getElementById("dial");
+    console.log(direction);
+    switch (direction) {
+        case "North":
+        case "N":
+            dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+            break;
+        case "NE":
+        case "NNE":
+        case "ENE":
+            dial.setAttribute("class", "ne");
+            break;
+        case "NW":
+        case "NNW":
+        case "WNW":
+            dial.setAttribute("class", "nw");
+            break;
+        case "South":
+        case "S":
+            dial.setAttribute("class", "s");
+            break;
+        case "SE":
+        case "SSE":
+        case "ESE":
+            dial.setAttribute("class", "se");
+            break;
+        case "SW":
+        case "SSW":
+        case "WSW":
+            dial.setAttribute("class", "sw");
+            break;
+        case "East":
+        case "E":
+            dial.setAttribute("class", "e");
+            break;
+        case "West":
+        case "W":
+            dial.setAttribute("class", "w");
+            break;
+    }
+}
+
+function getCondition(condition) {
+    const weatherCondition = document.getElementById("weatherCondition");
+    console.log(condition);
+    if ((condition.includes("Clear")) || (condition.includes("Sunny"))){
+        return "Clear";
+    }
+    else if ((condition.includes("Snow")) || (condition.includes("Flurries"))){
+        return "Snow";
+    }
+    else if ((condition.includes("Rain")) || (condition.includes("Showers")) || (condition.includes("Sprinkle")) || (condition.includes("Thunderstorm"))){
+        return "Rain";
+    }
+    else if ((condition.includes("Fog")) || (condition.includes("Dew")) || (condition.includes("Mist"))){
+        return "Fog"
+    }
+    else {
+        return "Clouds"
+    }
+}
+
+
+
+// function getCondition(condition) {
+//     const weatherCondition = document.getElementById("weatherCondition");
+//     console.log(condition);
+//     switch (condition) {
+//         case "Sunny":
+//         case "Clear":
+//             weatherCondition.setAttribute("class", "clear");
+//             break;
+//         case "Snow":
+//         case "Sleet":
+//         case "Flurries":
+//         case "Freezing Rain":
+//             weatherCondition.setAttribute("class", "snow");
+//             break;
+//         case "Rain":
+//         case "Showers":
+//         case "Thunderstorm":
+//         case "Wet":
+//             weatherCondition.setAttribute("class", "rain");
+//             break;
+//         case "Partly Cloudy":
+//         case "Partly Sunny":
+//         case "Cloudy":
+//         case "Overcast":
+//             weatherCondition.setAttribute("class", "clouds");
+//             break;
+//         case "Fog":
+//         case "Mist":
+//         case "Dew":
+//             weatherCondition.setAttribute("class", "fog");
+//             break;
+//     }
+// }
+
