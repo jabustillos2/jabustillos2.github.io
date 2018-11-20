da147a871aaa790de965eed066efb3eabe330ee5
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-11-20 15:27:01 -0700

    n

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -20,7 +20,10 @@ function getGeoLocation(){
          // Combine the values
          const LOCALE = LAT + "," + LONG;
          console.log(`Lat and Long are: ${LOCALE}.`);
-      
+         console.log(`Lat and Long are: ${LOCALE}.`);
+
+         // Call getCode function, send locale
+         getCode(LOCALE);
       
         })
        } else {
