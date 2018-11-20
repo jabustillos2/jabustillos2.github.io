8f0b62f133928a35700d6c82926eada18c97d6ef
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-11-20 15:22:42 -0700

    m

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -10,6 +10,18 @@ getGeoLocation();
 
 // Gets longitude and latitude of current location
 function getGeoLocation(){
-    const STATUS = document.getElementById('status');
-    STATUS.innerHTML = 'Getting Location...';
-} //end getGeoLocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+         const LAT = position.coords.latitude;
+         const LONG = position.coords.longitude;
+      
+         // Combine the values
+         const LOCALE = LAT + "," + LONG;
+         console.log(`Lat and Long are: ${LOCALE}.`);
+      
+      
+        })
+       } else {
+        STATUS.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
+       } // end else
+       //end getGeoLocation
