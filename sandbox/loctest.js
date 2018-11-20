507d34364716ca57372bbe968bfcb0a65e8d73f5
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-11-20 15:11:13 -0700

    js

---
+++ sandbox/loctest.js
@@ -0,0 +1,13 @@
+/*functions will work together to get weather info for current
+location and populate a web pg with data*/
+
+'use strict';
+
+
+
+
+
+// Gets longitude and latitude of current location
+function getGeoLocation(){
+
+} //end getGeoLocation
