2d62dd989cda335123a6f339b4e429e23b910e77
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-11-20 15:23:19 -0700

    n

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -10,6 +10,8 @@ getGeoLocation();
 
 // Gets longitude and latitude of current location
 function getGeoLocation(){
+    const STATUS = document.getElementById('status');
+    STATUS.innerHTML = 'Getting Location...';
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
          const LAT = position.coords.latitude;
@@ -24,4 +26,4 @@ function getGeoLocation(){
        } else {
         STATUS.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
        } // end else
-       //end getGeoLocation
+      } //end getGeoLocation
