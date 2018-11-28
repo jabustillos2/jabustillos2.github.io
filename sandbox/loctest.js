d16dde095066d99680d915f9e163145693b8e0c9
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-11-28 13:27:34 -0700

    h

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -29,5 +29,26 @@ function getGeoLocation(){
        } // end else
       } //end getGeoLocation
 
+// Get location code from API
+function getCode(LOCALE) {
+    const API_KEY = '2gUfqbpCyXDXEIku467zvS4vMg0HHxeb';
+    const URL = 'http:dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+API_KEY+'&q='+LOCALE;
+    fetch(URL)
+     .then(response => response.json())
+     .then(function (data) {
+      console.log('Json object from getCode function:');
+      console.log(data);
+      const locData = {}; // Create an empty object
+      locData['key'] = data.Key; // Add the value to the object
+      locData['name'] = data.LocalizedName;
+      locData['postal'] = data.PrimaryPostalCode;
+      locData['state'] = data.AdministrativeArea.LocalizedName;
+      locData['stateAbbr'] = data.AdministrativeArea.ID;
+      locData['geoposition'] = LOCALE;
+      locData['elevation'] = data.GeoPosition.Elevation.Imperial.Value;
+      getWeather(locData);
+      })
+     .catch(error => console.log('There was a getCode error: ', error))
+  } // end getCode function
       // URL to request city data using latitude and longitude
       // http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=2gUfqbpCyXDXEIku467zvS4vMg0HHxeb&q=43.816667%2C-111.783333&details=false&toplevel=false"
