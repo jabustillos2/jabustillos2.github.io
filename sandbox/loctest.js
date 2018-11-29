f8f5a1293eed7e91d01b5d271b92f16ed7dfe350
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-11-29 08:34:54 -0700

    t

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -33,7 +33,7 @@ function getGeoLocation(){
 function getCode(LOCALE) {
     const API_KEY = '2gUfqbpCyXDXEIku467zvS4vMg0HHxeb';
     const URL = 'http:dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+API_KEY+'&q='+LOCALE;
-    const TEMP = data[0].Temperature.Imperial.Value
+    
     fetch(URL)
      .then(response => response.json())
      .then(function (data) {
@@ -62,6 +62,7 @@ function getWeather(locData) {
     const API_KEY = '2gUfqbpCyXDXEIku467zvS4vMg0HHxeb';
     const CITY_CODE = locData['key']; // We're getting data out of the object
     const URL = "http://dataservice.accuweather.com/currentconditions/v1/332634?apikey="+CITY_CODE+"?apikey="+API_KEY+"&details=true";
+    const TEMP = data[0].Temperature.Imperial.Value
     fetch(URL)
      .then(response => response.json())
      .then(function (data) {
