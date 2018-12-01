8c23073c64f07ed2fe7b8f607f47cbbc7a4e2b16
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-12-01 12:46:49 -0700

    j

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -1,39 +1,33 @@
 /* *************************************
-*  Weather Site JavaScript Functions
-************************************* */
+ *  Weather Site JavaScript Functions
+ ************************************* */
 
-/* variables for function use*/
-const temp = 31;
-const speed = 5;
-buildWC(speed,temp);
 
-const direction = "N"; //Set your own value
-windDial(direction);
 
-const condition = "Mist";
-let currentCondition = getCondition(condition);
+// Variables for Function Use
+// const temp = 31;
+// const speed = 5;
+// buildWC(speed, temp);
 
-changeSummaryImage(currentCondition);
+// const direction = "N"; //Set your own value
+// windDial(direction);
 
-/* calculate windchill*/
+// const condition = "Sunny";
+// let currentCondition = getCondition(condition);
 
+// changeSummaryImage(currentCondition);
+
+// changeBackgroundImage(currentCondition);
+
+
+//Calculate wind chill temperature
 function buildWC(speed, temp) {
     const feelTemp = document.getElementById('feelTemp');
-
-    /*compute Windchill*/
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
-
-    /*round down integer*/
-    wc=Math.floor(wc);
-    
-/* If chill is greater than temp, return the temp*/
-    wc = (wc > temp)?temp:wc;
-
-/*display windchill*/
+    wc = Math.floor(wc);
+    wc = (wc > temp) ? temp : wc;
     console.log(wc);
-
-/* wc='feels like ' + wc+'F';*/
     feelTemp.innerHTML = wc;
 }
 
@@ -81,64 +75,34 @@ function windDial(direction) {
     }
 }
 
+
+
+//function to display weather keyword
 function getCondition(condition) {
     const weatherCondition = document.getElementById("weatherCondition");
-    console.log(condition);
-    if ((condition.includes("Clear")) || (condition.includes("Sunny")) || (condition.includes('clear')) || (condition.includes('sunny'))){
-        condition = "Clear";
+    if ((condition.includes("Clear")) || (condition.includes("Sunny")) || (condition.includes("clear")) || (condition.includes("sunny"))){
+        condition="Clear";
     }
-    else if ((condition.includes("Snow")) || (condition.includes("Flurries")) || (condition.includes('snow')) || (condition.includes('flurries'))){
-        condition = "Snow";
+    else if ((condition.includes("Snow")) || (condition.includes("Flurries")) || (condition.includes("snow")) || (condition.includes("flurries"))){
+        condition="Snow";
     }
     else if ((condition.includes("Rain")) || (condition.includes("Showers")) || (condition.includes("Sprinkle")) || (condition.includes("Thunderstorm")) || (condition.includes("rain")) || (condition.includes("showers")) || (condition.includes("sprinkle")) || (condition.includes("thunderstorm"))){
-        condition = "Rain";
+        condition= "Rain";
     }
-    else if ((condition.includes("Fog")) || (condition.includes("Dew")) || (condition.includes("Mist")) || (condition.includes('fog')) || (condition.includes('dew')) || (condition.includes('mist'))){
-        condition = "Fog"
+    else if ((condition.includes("Fog")) || (condition.includes("Dew")) || (condition.includes("Mist")) || (condition.includes("fog")) || (condition.includes("dew")) || (condition.includes("mist"))){
+        condition="Fog";
     }
-    else if ((condition.includes('Partly Cloudy')) || (condition.includes('Overcast')) || (condition.includes('Partly Sunny')) || (condition.includes('partly cloudy')) || (condition.includes('overcast')) || (condition.includes('partly sunny'))){
-        condition = "Clouds"
+    else if ((condition.includes("Partly Cloudy")) || (condition.includes("Overcast")) || (condition.includes("Partly Sunny")) || (condition.includes("partly cloudy")) || (condition.includes("overcast")) || (condition.includes("partly sunny"))){
+        condition="Clouds";
     }
     console.log(condition);
-    weatherCondition.innerHTML=condition;
+    weatherCondition.innerHTML=condition; 
     return condition;
+    
 }
 
 
 
-// function getCondition(condition) {
-//     const weatherCondition = document.getElementById("weatherCondition");
-//     console.log(condition);
-//     switch (condition) {
-//         case "Sunny":
-//         case "Clear":
-//             weatherCondition.setAttribute("class", "clear");
-//             break;
-//         case "Snow":
-//         case "Sleet":
-//         case "Flurries":
-//         case "Freezing Rain":
-//             weatherCondition.setAttribute("class", "snow");
-//             break;
-//         case "Rain":
-//         case "Showers":
-//         case "Thunderstorm":
-//         case "Wet":
-//             weatherCondition.setAttribute("class", "rain");
-//             break;
-//         case "Partly Cloudy":
-//         case "Partly Sunny":
-//         case "Cloudy":
-//         case "Overcast":
-//             weatherCondition.setAttribute("class", "clouds");
-//             break;
-//         case "Fog":
-//         case "Mist":
-//         case "Dew":
-//             weatherCondition.setAttribute("class", "fog");
-//             break;
-//     }
-// }
 
 
 // change the summary image
@@ -163,68 +127,57 @@ function changeSummaryImage(condition) {
             break;
     }
 }
-/*functions will work together to get weather info for current
-location and populate a web pg with data*/
 
-'use strict';
-// Call the function to get our location
-getGeoLocation();
-// Gets longitude and latitude of current location
-function getGeoLocation(){
-    const STATUS = document.getElementById('status');
-    STATUS.innerHTML = 'Getting Location...';
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-         const LAT = position.coords.latitude;
-         const LONG = position.coords.longitude;
-         
-         // Combine the values
-         const LOCALE = LAT + "," + LONG;
-         console.log(`Lat and Long are: ${LOCALE}.`);
-         // Call getCode function, send locale
-         getCode(LOCALE);
-      
-        })
-       } else {
-        STATUS.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
-       } // end else
-      } //end getGeoLocation
+    function changeBackgroundImage(condition) {
+    const curWeather = document.getElementById("curWeather");
+    console.log(condition);
+    switch (condition) {
+        case "Clear":
+            curWeather.setAttribute("class", "clear");
+            break;
+        case "Snow":
+            curWeather.setAttribute("class", "snow");
+            break;
+        case "Rain":
+            curWeather.setAttribute("class", "rain");
+            break;
+        case "Clouds":
+            curWeather.setAttribute("class", "clouds");
+            break;
+        case "Fog":
+            curWeather.setAttribute("class", "fog");
+            break;
+    }
+}
 
 // Get location code from API
 function getCode(LOCALE) {
     const API_KEY = '2gUfqbpCyXDXEIku467zvS4vMg0HHxeb';
     const URL = 'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey='+API_KEY+'&q='+LOCALE;
-    
     fetch(URL)
-      .then(response => response.json())
-      .then(function (data) {
-         console.log('Json object from getCode function:');
-         console.log(data);
-         const locData = {}; // Create an empty object
-         locData['key'] = data.Key; // Add the value to the object
-         locData['name'] = data.LocalizedName;
-         locData['postal'] = data.PrimaryPostalCode;
-         locData['state'] = data.AdministrativeArea.LocalizedName;
-         locData['stateAbbr'] = data.AdministrativeArea.ID;
-         locData['geoposition'] = LOCALE;
-         locData['elevation'] = data.GeoPosition.Elevation.Imperial.Value;
-         getWeather(locData);
+     .then(response => response.json())
+     .then(function (data) {
+      console.log('Json object from getCode function:');
+      console.log(data);
+      const locData = {}; // Create an empty object
+      locData['key'] = data.Key; // Add the value to the object
+      locData['name'] = data.LocalizedName;
+      locData['postal'] = data.PrimaryPostalCode;
+      locData['state'] = data.AdministrativeArea.LocalizedName;
+      locData['stateAbbr'] = data.AdministrativeArea.ID;
+      locData['geoposition'] = LOCALE;
+      locData['elevation'] = data.GeoPosition.Elevation.Imperial.Value;
+      getWeather(locData);
       })
      .catch(error => console.log('There was a getCode error: ', error))
   } // end getCode function
-      // URL to request city data using latitude and longitude
-      // https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=2gUfqbpCyXDXEIku467zvS4vMg0HHxeb&q=43.816667%2C-111.783333&details=false&toplevel=false"
 
 
-
-      //https://dataservice.accuweather.com/currentconditions/v1/332634?apikey=2gUfqbpCyXDXEIku467zvS4vMg0HHxeb&details=true"
-      // Get Current Weather data from API
+  // Get Current Weather data from API
 function getWeather(locData) {
     const API_KEY = '2gUfqbpCyXDXEIku467zvS4vMg0HHxeb';
     const CITY_CODE = locData['key']; // We're getting data out of the object
     const URL = "https://dataservice.accuweather.com/currentconditions/v1/"+CITY_CODE+"?apikey="+API_KEY+"&details=true";
-    
-    // const TEMP = data[0]['Temperature']['Imperial']['Value'];
     fetch(URL)
      .then(response => response.json())
      .then(function (data) {
@@ -243,6 +196,7 @@ function getWeather(locData) {
       })
      .catch(error => console.log('There was an error: ', error))
   } // end getWeather function
+
   // Get next 12 hours of forecast data from API
 function getHourly(locData) {
     const API_KEY = '2gUfqbpCyXDXEIku467zvS4vMg0HHxeb';
@@ -335,32 +289,33 @@ function getHourly(locData) {
 
 // formats a value into a 12h AM/PM time string
 function format_time(hour) {
-   if (hour > 23) {
-      hour -= 24;
-   }
-   let amPM = (hour > 11) ? "pm" : "am";
-   if (hour > 12) {
-      hour -= 12;
-   } else if (hour == 0) {
-      hour = "12";
-   }
-   return hour + amPM;
-} // end format_time function
-
-function buildHourly(locData) {
-   //const hourlyTime = document.getElementById("forecast").children[1];
-   const hourlyTime = document.createElement("ul");
-   console.log(hourlyTime);
-
-   const currentHour = new Date().getHours();
-   console.log(currentHour);
-
-   for (let i = 0; i < 12; i++) {
-      const newHour = document.createElement('li');
-      newHour.innerHTML = `${format_time(currentHour - i)}: ${locData["hourTemp" + (i + 1)]}&deg;F`;
-      hourlyTime.appendChild(newHour);
-   }
-   console.log(hourlyTime);
-   const forecast = document.getElementById("hourly");
-   forecast.replaceChild(hourlyTime, forecast.children[1]);
-}
+    if (hour > 23) {
+       hour -= 24;
+    }
+    let amPM = (hour > 11) ? "pm" : "am";
+    if (hour > 12) {
+       hour -= 12;
+    } else if (hour == 0) {
+       hour = "12";
+    }
+    return hour + amPM;
+ } // end format_time function
+ 
+ function buildHourly(locData) {
+    //const hourlyTime = document.getElementById("forecast").children[1];
+    const hourlyTime = document.createElement("ul");
+    console.log(hourlyTime);
+ 
+    const currentHour = new Date().getHours();
+    console.log(currentHour);
+ 
+    for (let i = 0; i < 12; i++) {
+       const newHour = document.createElement('li');
+       newHour.innerHTML = `${format_time(currentHour - i)}: ${locData["hourTemp" + (i + 1)]}&deg;F`;
+       hourlyTime.appendChild(newHour);
+    }
+    console.log(hourlyTime);
+    const forecast = document.getElementById("hourly");
+    forecast.replaceChild(hourlyTime, forecast.children[1]);
+ }
+ 
