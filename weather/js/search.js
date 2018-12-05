003fc0ebc5edcd1c3a8bd19329b4348181159dc9
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-12-05 13:32:25 -0700

    n

--- weather/js/search.js
+++ weather/js/search.js
@@ -9,3 +9,31 @@ QUERY.addEventListener("keyup", function () {
     // Call the processJSON function to request data and build results
     processJSON(searchValue);
    }); // ends the eventListener
+
+   // Request data and build the list of matching locations
+function processJSON(searchValue) {
+    // Get Data from the Autocomplete API
+    const API_KEY = '2gUfqbpCyXDXEIku467zvS4vMg0HHxeb';
+    let URL = "https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=" + API_KEY + "&q=" + searchValue;
+    
+    fetch(URL)
+    .then(response => response.json())
+    .then(function (data) {
+    console.log('Json object from autocomplete API:');
+    console.log(data); // Log what is returned
+      
+      // Build a list of returned locations
+      let list = '<ul>';
+      for (let i = 0, n = data.length; i < n; i++) {
+        let cityKey = data[i].Key;
+        let cityName = data[i].LocalizedName;
+        let stateCode = data[i].AdministrativeArea.ID;
+        let locationName = cityName+', '+stateCode;
+        list += "<li><a data-key='" + cityKey + "' href='https://www.accuweather.com/ajax-service/select-city?cityId=" + cityKey + "&lang=en-us' title='See weather information for " + locationName + "' target='_blank'>" + locationName + "</a></li>";
+      };
+      list += '</ul>';
+      // Inject the list to the search page
+      let searchResults = document.getElementById("searchResults");
+      searchResults.innerHTML = list;
+    })  .catch(error => console.log('There was an error: ', error))
+  } // ends the processJSON function
