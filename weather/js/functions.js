be5ca9b5c6f1c0efd74fde72a8b1c62cb3458431
Bustillos Laptop <busfamlaptop@Bustilloss-MacBook-Air.local>
2018-12-05 13:40:46 -0700

    a

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -319,4 +319,29 @@ function format_time(hour) {
     const forecast = document.getElementById("hourly");
     forecast.replaceChild(hourlyTime, forecast.children[1]);
  }
+
+ // Get location info, based on city key, from API
+function getLocationByKey(cityKey) {
+    const API_KEY = '2gUfqbpCyXDXEIku467zvS4vMg0HHxeb';
+    const URL = 'https://dataservice.accuweather.com/locations/v1/'+cityKey+'?apikey='+API_KEY;
+    fetch(URL)
+     .then(response => response.json())
+     .then(function (data) {
+      console.log('Json object from getLocationByKey function:');
+      console.log(data);
+      const locData = {}; // Create an empty object
+      locData['key'] = data.Key; // Add the value to the object
+      locData['name'] = data.LocalizedName;
+      locData['postal'] = data.PrimaryPostalCode;
+      locData['state'] = data.AdministrativeArea.LocalizedName;
+      locData['stateAbbr'] = data.AdministrativeArea.ID;
+      let lat = data.GeoPosition.Latitude;
+      let long = data.GeoPosition.Longitude;
+      const LOCALE = lat+', '+long;
+      locData['geoposition'] = LOCALE;
+      locData['elevation'] = data.GeoPosition.Elevation.Imperial.Value;
+      getWeather(locData);
+      })
+     .catch(error => console.log('There was a getLocationByKey error: ', error))
+    } // end getLocationByKey function
  
